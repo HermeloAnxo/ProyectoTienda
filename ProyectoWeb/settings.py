7d3b84b80df591965a8fe7b0f17a3013fc145170
Anxobross <anxo.hermelo.2002@gmail.com>
2023-09-13 01:07:15 +0000

    cambios en BBDD

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q9@@$1a5wbd^$&0_r%*f5mh!vfydrz-b+hma$^r^pr^hefj#xf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Anxobross.pythonanywhere.com']
 
 
 # Application definition
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'ProyectoWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'Anxobross$productos',
+        'USER' : 'Anxobross',
+        'PASSWORD' : 'Anxo2002!',
+        'HOST' : 'Anxobross.mysql.pythonanywhere-services.com',
+        'PORT' : '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
